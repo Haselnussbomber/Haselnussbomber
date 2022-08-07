a0493de8cb0c441735e5a5db7b29c4e52a8aa4e9
Haselnussbomber <mail@haselnussbomber.de>
2022-08-07 15:12:56 +0200

    add ko-fi button

--- main.ts
+++ main.ts
@@ -129,6 +129,8 @@ await Deno.writeTextFile("README.md", `## Hey there!
 
 These are a bunch of small and simple Addons/Plugins that I wrote. Have fun!
 
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/A0A7E5DE5)
+
 ### Final Fantasy XIV Plugins
 
 ${repos.filter(byTag("ffxiv")).map(formatRepo).join("\n").trim()}
