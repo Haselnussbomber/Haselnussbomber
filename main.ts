b2258b03029a63d774d1b04e98103ee6abee72c3
Haselnussbomber <mail@haselnussbomber.de>
2023-05-29 03:57:13 +0200

    i forgot this script existed

--- main.ts
+++ main.ts
@@ -125,9 +125,13 @@ const escapeHtml = (str: string) => str
 const formatRepo = (repo: Repository) =>
   `#### <a href="${repo.html_url}" title="${escapeHtml(repo.description)}">${repo.name}</a> <span title="Released ${formatDate(repo.latestRelease.published_at)}">${repo.latestRelease.tag_name}</span>\n\n${repo.description}\n`;
 
-await Deno.writeTextFile("README.md", `## Hey there!
+await Deno.writeTextFile("README.md", `## Heya!
 
-These are a bunch of small and simple Addons/Plugins that I wrote. Have fun!
+It's me, Alexander, a shy hobby-coder from Germany.
+
+I mainly create small FFXIV plugins and WoW addons for my personal use. Taking the step to share them on GitHub is a big deal for me, but I believe my creations could be valuable to others as well and maybe some ideas will even end up in the games themselves one day. ![Prayge](https://github.com/Haselnussbomber/Haselnussbomber/assets/96642047/18fc83b1-56a1-4354-94d1-9070eacb7f07)
+
+I hope you find them as helpful as I do, so please enjoy using them! :)
 
 [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/A0A7E5DE5)
 
