d72fe93ac8d5539c2c65e980d0b925a523c413e2
Haselnussbomber <mail@haselnussbomber.de>
2023-07-02 21:35:52 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -127,11 +127,11 @@ const formatRepo = (repo: Repository) =>
 
 await Deno.writeTextFile("README.md", `## Heya!
 
-It's me, Alexander, a shy hobby-coder from Germany.
+I'm Alexander, a shy hobby-coder from Germany.
 
-I mainly create small FFXIV plugins and WoW addons for my personal use. Taking the step to share them on GitHub is a big deal for me, but I believe my creations could be valuable to others as well and maybe some ideas will even end up in the games themselves one day. ![Prayge](https://github.com/Haselnussbomber/Haselnussbomber/assets/96642047/18fc83b1-56a1-4354-94d1-9070eacb7f07)
+I mainly create small FFXIV plugins and WoW addons for my personal use and thought I'd share them in case someone else finds them as useful as I do. And who knows? Maybe some of the ideas will even end up in the games themselves one day. ![Prayge](https://github.com/Haselnussbomber/Haselnussbomber/assets/96642047/18fc83b1-56a1-4354-94d1-9070eacb7f07)
 
-I hope you find them as helpful as I do, so please enjoy using them! :)
+Enjoy! :)
 
 [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/A0A7E5DE5)
 
